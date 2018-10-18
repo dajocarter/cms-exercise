5cd80e2d1f8ec6e2a4226748435264b1d6f53f22
David Carter <dajocarter@gmail.com>
2018-10-17 20:43:45 -0700

    Add asterisks

--- tinymce-giphy/index.php
+++ tinymce-giphy/index.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: TinyMCE Giphy
-Plugin URI: https://github.com/dajocarter/cms-exercise
-Description: Adds a button to the TinyMCE editor to allow authors to search for gifs to insert into their articles
-Version: 1.0
-Author: David Carter
-Author URI: https://www.dajocarter.com/
-*/
+ * Plugin Name: TinyMCE Giphy
+ * Plugin URI: https://github.com/dajocarter/cms-exercise
+ * Description: Adds a button to the TinyMCE editor to allow authors to search for gifs to insert into their articles
+ * Version: 1.0
+ * Author: David Carter
+ * Author URI: https://www.dajocarter.com/
+ */
 
 ?>
