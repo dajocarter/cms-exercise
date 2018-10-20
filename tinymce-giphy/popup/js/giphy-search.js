7db843e3475f1c38ab22752ed9c77c7d2fa16d20
David Carter <dajocarter@gmail.com>
2018-10-19 20:28:02 -0700

    Create a function to fetch giphy api

--- tinymce-giphy/popup/js/giphy-search.js
+++ tinymce-giphy/popup/js/giphy-search.js
@@ -6,4 +6,16 @@ const GiphySearch = {
   query: "",
   offset: 0,
   limit: 24,
+  search: function(endpoint, query) {
+    // Configure URL to fetch
+    let fetchURL = `https://api.giphy.com/v1/gifs/${
+      this.endpoint
+    }?api_key=2C40RpauBXJXFyrVrwNjjFSGGHlVVObw&limit=${this.limit}`;
+    if (this.query !== "") fetchURL += `&q=${this.query}`;
+    if (this.offset > 0) fetchURL += `&offset=${this.offset}`;
+    // Fetch the results
+    return fetch(fetchURL)
+      .then(resp => resp.json())
+      .catch(err => console.log(err));
+  }
 };
