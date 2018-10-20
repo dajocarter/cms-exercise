4f90d6f74ee11c49ba98d052a3e12be8995b546a
David Carter <dajocarter@gmail.com>
2018-10-19 21:00:37 -0700

    Write a function to render the giphy data

--- tinymce-giphy/popup/js/giphy-search.js
+++ tinymce-giphy/popup/js/giphy-search.js
@@ -6,6 +6,30 @@ const GiphySearch = {
   query: "",
   offset: 0,
   limit: 24,
+  renderGifs: function(resp) {
+    // Grab the UL
+    const gifsContainer = document.getElementById("gifs");
+    // Empty out the UL if a new search
+    if (resp.pagination && resp.pagination.offset === 0) {
+      gifsContainer.innerHTML = "";
+    }
+    // Create an LI for each gif and add it to the list
+    resp.data &&
+      resp.data.map(gif => {
+        // Create elements to inject
+        let li = document.createElement("li"),
+          img = document.createElement("img");
+        // Set src for gif (apparently, fixed_height_small url doesn't exist for all gifs)
+        img.src = gif.images.fixed_height_small.url
+          ? gif.images.fixed_height_small.url
+          : gif.images.fixed_height.url;
+        // Set data-gif-id for use in iframe src url when embedding gif
+        img.dataset.gifId = gif.id;
+        // Append IMG to LI and LI to UL
+        li.appendChild(img);
+        gifsContainer.appendChild(li);
+      });
+  },
   /**
    * Returns request from GIPHY
    * @param {string} [url] - Endpoint to request. Should be empty to keep current request terms. Otherwise, must be one of 'search' or 'trending'.
@@ -22,6 +46,7 @@ const GiphySearch = {
     // Fetch the results
     return fetch(fetchURL)
       .then(resp => resp.json())
+      .then(resp => this.renderGifs(resp))
       .catch(err => console.log(err));
   }
 };
