6453fd6ec545d4662a84d1ca25ea2a882bc67b81
David Carter <dajocarter@gmail.com>
2018-10-21 11:27:56 -0700

    Create template files for pages and posts

---
+++ frontend/src/templates/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const PageTemplate = () => {
+  return (
+    <Layout>
+      <h1>Hello from the page template.</h1>
+    </Layout>
+  )
+}
+
+export default PageTemplate
---
+++ frontend/src/templates/post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const PostTemplate = () => {
+  return (
+    <Layout>
+      <h1>Hello from the post template.</h1>
+    </Layout>
+  )
+}
+
+export default PostTemplate
