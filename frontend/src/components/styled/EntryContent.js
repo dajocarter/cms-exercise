3c6aabc56da8201245bd1f66819ebfc44b059333
David Carter <dajocarter@gmail.com>
2018-10-22 21:34:25 -0700

    Responsive giphys

--- frontend/src/components/styled/EntryContent.js
+++ frontend/src/components/styled/EntryContent.js
@@ -1,10 +1,21 @@
 import styled from 'styled-components'
 
 const EntryContent = styled.div`
-  iframe.giphy-embed {
-    display: block;
-    margin: 0 auto;
+  .embed-container {
+    position: relative;
+    margin-bottom: 1.5rem;
+    padding-bottom: 56.25%;
+    overflow: hidden;
     max-width: 100%;
+    height: auto;
+
+    .giphy-embed {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
   }
 `
 
