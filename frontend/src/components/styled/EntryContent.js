91d9efc6e33fc3e0d2a744b6200bc48d9d338ef7
David Carter <dajocarter@gmail.com>
2018-10-22 21:15:12 -0700

    Center iframe giphs

--- frontend/src/components/styled/EntryContent.js
+++ frontend/src/components/styled/EntryContent.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 
-const EntryContent = styled.div``
+const EntryContent = styled.div`
+  iframe.giphy-embed {
+    display: block;
+    margin: 0 auto;
+    max-width: 100%;
+  }
+`
 
 export default EntryContent
